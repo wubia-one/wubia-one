33edd8dcee8f0d5d22328da759e7b8fae1933a27
wubia-one <k3vlpHD@gmail.com>
2023-11-13 14:19:50 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,7 +10,11 @@ const projects: Project[] = [
     title: "Dotfiles",
     techs: ["bash, zsh"],
     link: "https://github.com/wubia-one/Dotfiles"}
-  
+    ,{
+      title: "",
+      techs:["JavaScripts"],
+      link:"https://files.yande.re/sample/1e548aee19a15c80c0a78683870059e1/yande.re%201128335%20sample%20angel%20blue_archive%20dress%20hayase_yuuka%20ichinose_asuna%20misono_mika%20mossi%20open_shirt%20pantyhose%20seifuku%20shimoe_koharu%20tendou_arisu%20wings%20yuri.jpg"
+    }
 ];
 
 export default projects;
