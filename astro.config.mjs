68e5e9592c56b12826c4f9fc3ce3e4407d54d33b
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-14 10:13:14 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt()prefetch({
+    intentSelector: ["a[href^='/wubia-one']", "a[href^='/wubia-one/cv']", "a[href^='/wubia-one/posts']", "a[href^='/wubia-one/posts/Buisness']", "a[href^='/wubia-one/posts/Dotfiles']", "a[href^='/wubia-one/posts/untitled%202']", "a[href^='/wubia-one/posts/untitled']", "a[href^='/wubia-one/images']"
+  ],
+  })],
   site: 'https://wubia-one.github.io',
   base: '/wubia-one',
   markdown: {
