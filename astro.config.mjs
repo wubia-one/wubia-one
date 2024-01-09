a50ff9673615432efb036a094d2dd5259c796ee1
wubia-one <k3vlpHD@gmail.com>
2024-01-09 14:15:39 +0100

    xx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,12 @@ import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-
+import cloudflare from '@astrojs/cloudflare';
 import netlify from "@astrojs/netlify/functions";
-//import netlify from '@astrojs/netlify/edge-functions';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), prefetch({
-    intentSelector: ["a[href^='/']", "a[href^='/cv']", "a[href^='/impressum']", "a[href^='/posts']", "a[href^='/posts/Buisness']", "a[href^='/posts/Dotfiles']", "a[href^='/posts/untitled%202']", "a[href^='/posts/untitled']", "a[href^='/wubia-one/images']"]
-  }), react()],
-  site: 'https://wubia.netlify.app',
-  //base: '/wubia-one',
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false
-    }
-  },
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: cloudflare(),
+  // ...
+  integrations: [tailwind()],
 });
